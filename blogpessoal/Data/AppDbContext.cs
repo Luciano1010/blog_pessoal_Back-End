4dfc73d2e899aed41d7a008d5afc1ee6cdce90eb
Luciano Simões <138714631+Luciano1010@users.noreply.github.com>
2023-12-06 11:12:17 -0300

    Update AppDbContext.cs

--- blogpessoal/Data/AppDbContext.cs
+++ blogpessoal/Data/AppDbContext.cs
@@ -33,35 +33,37 @@ namespace blogpessoal.Data
         public DbSet<Postagem> Postagens { get; set; } = null!; // registrar  Dbset - Obejto responsavel por manipular a Tabela
         public DbSet<Tema> Temas { get; set; } = null!; // objeto responsavel por manipular os temas, sendo usado na posatagem service no Getall.
         public DbSet<User> Users { get; set; } = null!;
-        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        
+         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             var insertedEntries = this.ChangeTracker.Entries()
-                                   .Where(x => x.State == EntityState.Added)
-                                   .Select(x => x.Entity);
+                .Where(x => x.State == EntityState.Added)
+                .Select(x => x.Entity);
 
             foreach (var insertedEntry in insertedEntries)
             {
                 //Se uma propriedade da Classe Auditable estiver sendo criada. 
                 if (insertedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now, new TimeSpan(-3, 0, 0));
+                    auditableEntity.Data = DateTimeOffset.Now;
                 }
             }
 
             var modifiedEntries = ChangeTracker.Entries()
-                       .Where(x => x.State == EntityState.Modified)
-                       .Select(x => x.Entity);
+                    .Where(x => x.State == EntityState.Modified)
+                    .Select(x => x.Entity);
 
             foreach (var modifiedEntry in modifiedEntries)
             {
                 //Se uma propriedade da Classe Auditable estiver sendo atualizada.  
                 if (modifiedEntry is Auditable auditableEntity)
                 {
-                    auditableEntity.Data = new DateTimeOffset(DateTime.Now, new TimeSpan(-3, 0, 0));
+                    auditableEntity.Data = DateTimeOffset.Now;
                 }
             }
 
             return base.SaveChangesAsync(cancellationToken);
         }
+
     }
 }
